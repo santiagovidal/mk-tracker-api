d59d4a4dcf2c510673865cb90672ce69b1a0103a
Santiago Vidal <santiago.vidal@rootstrap.com>
2019-02-20 15:09:47 -0300

    Improve championship endpoint specs

--- spec/requests/api/v1/championships/create_spec.rb
+++ spec/requests/api/v1/championships/create_spec.rb
@@ -8,36 +8,67 @@ describe 'POST api/v1/championships', type: :request do
   let!(:user3)  { create(:user) }
   let!(:user4)  { create(:user) }
 
-  let(:params) do
-    {
-      user_results_attributes: [
-        { user_id: user1.id, track_id: track1.id, position: 2 },
-        { user_id: user2.id, track_id: track1.id, position: 4 },
-        { user_id: user3.id, track_id: track1.id, position: 3 },
-        { user_id: user4.id, track_id: track1.id, position: 1 },
-        { user_id: user1.id, track_id: track2.id, position: 1 },
-        { user_id: user2.id, track_id: track2.id, position: 3 },
-        { user_id: user3.id, track_id: track2.id, position: 4 },
-        { user_id: user4.id, track_id: track2.id, position: 2 },
-      ]
-    }
-  end
   let(:subject) { post api_v1_championships_path, params: params, as: :json }
 
-  it 'returns success' do
-    subject
-    expect(response).to have_http_status(:no_content)
+  context 'with correct params' do
+    let(:params) do
+      {
+        user_results_attributes: [
+          { user_id: user1.id, track_id: track1.id, position: 2 },
+          { user_id: user2.id, track_id: track1.id, position: 4 },
+          { user_id: user3.id, track_id: track1.id, position: 3 },
+          { user_id: user4.id, track_id: track1.id, position: 1 },
+          { user_id: user1.id, track_id: track2.id, position: 1 },
+          { user_id: user2.id, track_id: track2.id, position: 3 },
+          { user_id: user3.id, track_id: track2.id, position: 4 },
+          { user_id: user4.id, track_id: track2.id, position: 2 },
+        ]
+      }
+    end
+
+    it 'returns success' do
+      subject
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it 'creates a new championship' do
+      expect do
+        subject
+      end.to change(Championship, :count).by(1)
+    end
+
+    it 'creates new user result records' do
+      expect do
+        subject
+      end.to change(UserResult, :count).by(8)
+    end
   end
 
-  it 'creates a new championship' do
-    expect do
-      subject
-    end.to change(Championship, :count).by(1)
-  end
+  context 'with incorrect params' do
+    let(:params) do
+      {
+        user_results_attributes: [
+          { user_id: user1.id, track_id: track1.id },
+          { user_id: user2.id, track_id: track1.id, position: 4 },
+          { user_id: user3.id, position: 3 },
+          { user_id: user4.id, track_id: track1.id, position: 1 },
+          { user_id: user1.id, track_id: track2.id, position: 1 },
+          { user_id: user2.id, track_id: track2.id, position: 3 },
+          { track_id: track2.id, position: 4 },
+          { user_id: user4.id, track_id: track2.id, position: 2 },
+        ]
+      }
+    end
 
-  it 'creates new user result records' do
-    expect do
+    it 'returns bad request' do
       subject
-    end.to change(UserResult, :count).by(8)
+      expect(response).to have_http_status(:bad_request)
+    end
+
+    it 'does not create a new championship' do
+      expect do
+        subject
+      end.to_not change(Championship, :count)
+    end
   end
 end
